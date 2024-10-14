ba3249ae086cc9261fe71617081693fd6b2ec34a
Prowler14 <guohao.k2@outlook.com>
2024-10-14 15:39:27 +0800

    Update Week1Lab9.java

--- Week1Lab9.java
+++ Week1Lab9.java
@@ -7,9 +7,15 @@ public class Week1Lab9 {
         int n1 = 2, n2 = 14;
         System.out.println("Welcome to Java Programming, " + name);
         int sum = n1 + n2;
-        System.out.println(sum);
+        n1 = shit(n1);
+        System.out.println(n1);
         System.out.println("The sum of 2 values is " + sum);
         //one of the numbers is float in an operation, will return float result
+    }
+
+    public static int shit(n1) {
+        n1++;
+        return n1;
     }
     
 }

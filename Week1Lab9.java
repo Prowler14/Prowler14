e0ff6ea4851d1011a49e9b0bdcb81e092a5c7a25
Prowler14 <guohao.k2@outlook.com>
2024-10-14 15:37:27 +0800

    Add files via upload

---
+++ Week1Lab9.java
@@ -0,0 +1,15 @@
+package week1lab9;
+
+public class Week1Lab9 {
+
+    public static void main(String[] args) {
+        String name = "Khoo";
+        int n1 = 2, n2 = 14;
+        System.out.println("Welcome to Java Programming, " + name);
+        int sum = n1 + n2;
+        System.out.println(sum);
+        System.out.println("The sum of 2 values is " + sum);
+        //one of the numbers is float in an operation, will return float result
+    }
+    
+}
